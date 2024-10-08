0f6f5b31c3d65b7110818b384ca79592f0b8e285
dharmik48 <hingudharmik4@gmail.com>
2024-10-08 20:10:10 +0530

    fix: hide dialog on all files removed

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-center p-24 gap-10'>
-			{!!files ? (
+			{!!files && files.length ? (
 				<UploadForm files={files} setFiles={setFiles} />
 			) : (
 				<>
